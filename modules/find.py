b53b43edeed2b7eda6dd569062d77824d513abdb
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-25 20:13:26 +0100

    Add a function to allow process's to be able to be found by the name

--- modules/find.py
+++ modules/find.py
@@ -25,3 +25,34 @@ class find:
             return process_information
         except Exception as error:
             return {"error": error}
+        
+    def by_name(process_name: str) -> Union[dict[str], None]:
+        """Get information about a process via its name.
+
+        Args:
+            process_name (str): Name of the process you want to find.
+
+        Returns:
+            Union[dict[str], None]: Returns a dict containing information or None.
+        """
+        try:
+            for process in psutil.process_iter(['pid', 'name']):
+                if process.name == process_name:
+                    process_result: object = psutil.Process(pid=process.pid)
+            
+                    process_information: dict[str] = {
+                        'name': process_result.name(),
+                        'status': process_result.status(),
+                        'memory_info': process_result.memory_info(),
+                        'cpu_percent': process_result.cpu_percent()    
+                    }
+                    
+                    return process_information
+                else:
+                    continue
+                
+            return None
+                
+        except Exception as error:
+            return {"error": error}
+        
